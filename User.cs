27428a5a0710f13991e9e2114cad8e8261757010
Eduardo Santos <139821874+Dudev-fy@users.noreply.github.com>
2023-09-13 00:54:48 -0300

    Delete User.cs

--- User.cs
+++
@@ -1,9 +0,0 @@
-public class User
-{   
-    public int IDUSER {get; set;}
-    public string NAME {get; set;}
-    public int ID_ACCOUNT {get; set;}
-    public ICollection<Statement> STATEMENTS {get; set;}
-    public ICollection<Statement> STATEMENTD {get; set;}
-
-}
