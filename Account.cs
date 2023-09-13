b1c6b214d876bd15052e6e2d4f9505b35d44e594
Eduardo Santos <139821874+Dudev-fy@users.noreply.github.com>
2023-09-13 00:53:37 -0300

    Delete Account.cs

--- Account.cs
+++
@@ -1,8 +0,0 @@
-public class Account
-{   
-    public int IDACCOUNT {get; set;}
-    public string ACCOUNT_NUMBER {get; set;}
-    public string PASSWORD {get; set;}
-    public double BALANCE {get; set;}
-    public Salt SALT {get; set;}
-}
