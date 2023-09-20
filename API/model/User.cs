6a583aabf1d4cd469891f64eb6d8558532cd8201
Eduardo Santos <139821874+Dudev-fy@users.noreply.github.com>
2023-09-20 19:17:44 -0300

    Update User.cs

--- API/model/User.cs
+++ API/model/User.cs
@@ -2,7 +2,7 @@ public class User
 {   
     public int IDUSER {get; set;}
     public string NAME {get; set;}
-    public int ID_ACCOUNT {get; set;}
+    public int? ID_ACCOUNT {get; set;}
     public ICollection<Statement> STATEMENTS {get; set;}
     public ICollection<Statement> STATEMENTD {get; set;}
 
